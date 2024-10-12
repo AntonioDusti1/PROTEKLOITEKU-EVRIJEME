6de49981a8bd5c235a21d27b21984f58f00c829b
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2024-10-12 12:09:53 +0200

    Add files via upload

---
+++ PROTEKLO I TEKUĆE.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROTEKLO_I_TEKUĆE
+{
+    public partial class Form1 : Form
+    {
+        private DateTime pocetno;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pocetno = DateTime.Now; 
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel2.Text = "Trenutno: " + DateTime.Now.ToLongTimeString();
+            TimeSpan proteklo = DateTime.Now - pocetno;
+            toolStripStatusLabel1.Text = "Proteklo: " +
+                proteklo.Hours.ToString("D2") + ":" +
+                proteklo.Minutes.ToString("D2") + ":" +
+                proteklo.Seconds.ToString("D2");
+        }
+    }
+}
